9c5d1f15a0a38603fcb250e08c1a9f95fee0c0ef
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-02 10:47:43 +0700

    selesai

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -93,9 +93,16 @@ void mergeSort(int low, int high)
 void output()
 {
     cout << "\nData setelah diurutkan (Merge Sort): ";
-    for (in i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
 }
