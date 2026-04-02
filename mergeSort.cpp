b8b1b064952626f55b31d77aab78bd5e30285aae
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-02 10:31:32 +0700

    menambahkan rumus array

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -26,5 +26,9 @@ void input()
     cout << "\nInputkan Isi element array" << endl;
     cout << "\n--------------------------" << endl;
 
-    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
